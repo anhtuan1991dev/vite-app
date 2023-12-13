d2fdf2d4642921855ec57a874f0e3431cb2d8b6e
kimori01 <anhtuan1991qn@gmail.com>
2023-12-13 17:25:12 +0700

    tuan update

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,30 +1,43 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import Row from '~/components/Row'
 import { Input, Label, Button, FormGroup } from '~/components/Forms'
 
 function DashboardPage() {
+  const schema = yup
+    .object()
+    .shape({
+      firstName: yup.string().required('Vui lòng nhập firstName'),
+      lastName: yup.string().required('Vui lòng nhập lastName')
+    })
+    .required()
+
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = useForm({ resolver: yupResolver(schema) })
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
 
   return (
     <Row>
       <div className='basis-full p-5 border border-gray-300'>
-        <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <FormGroup>
             <Label name='First name' />
-            <Input {...register('firstName', { required: true })} />
-            {errors.firstName && <p className='text-red-500'>First name is required.</p>}
+            <Input {...register('firstName')} />
+            {errors.firstName && <p className='text-red-500'>{errors.firstName.message}</p>}
           </FormGroup>
 
           <FormGroup>
             <Label name='Last name' />
-            <Input {...register('lastName', { required: true })} />
-            {errors.lastName && <p className='text-red-500'>Last name is required.</p>}
+            <Input {...register('lastName')} />
+            {errors.lastName && <p className='text-red-500'>{errors.lastName.message}</p>}
           </FormGroup>
-
           <Button name='Gui' type='submit' />
         </form>
       </div>
